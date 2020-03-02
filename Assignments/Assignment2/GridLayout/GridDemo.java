92ded33660cd7775331cafaea2c427c9c5172427
arjunQ21 <be2018se604@gces.edu.np>
2020-03-02 20:11:58 +0545

    GridLayout Demo

---
+++ Assignments/Assignment2/GridLayout/GridDemo.java
@@ -0,0 +1,16 @@
+import java.awt.* ;
+
+class GridDemo{
+	public static void main(String[] args){
+		Frame f = new Frame("Grid Layout Demo") ;
+		Button buttons[] = new Button[9] ;
+		int i = 1 ;
+		for(Button button: buttons){
+			button = new Button("Button " + i++) ;
+			f.add(button) ;
+		}
+		f.setLayout(new GridLayout(3, 3));
+		f.setSize(400, 400) ;
+		f.setVisible(true) ;
+	}
+}
